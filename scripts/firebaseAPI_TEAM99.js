d2a45465047ad147ac95d176bcff8ce5bd3258ac
Alex Choi <hchoi117@my.bcit.ca>
2024-02-20 13:03:40 -0800

    Updated demo07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyA_LB4l1qZvDWQZyRPusICbVY6mhhur-AE",
+    authDomain: "comp1800-202410-18c78.firebaseapp.com",
+    projectId: "comp1800-202410-18c78",
+    storageBucket: "comp1800-202410-18c78.appspot.com",
+    messagingSenderId: "1033020767455",
+    appId: "1:1033020767455:web:bacfe1b90ad072c4ce7d6a",
+    measurementId: "G-XS30E4F29E"
 };
 
 //--------------------------------------------
